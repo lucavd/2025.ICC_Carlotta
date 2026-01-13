8895e58556179a3b373529f250a51cc893a1c552
Luca Vedovelli <lucavd@gmail.com>
2026-01-13 22:57:21 +0100

    Remove obsolete monitor scripts

--- optimized/launch_monitor.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-title Monitor Progresso ICC
-cd /d %~dp0
-powershell -ExecutionPolicy Bypass -File "%~dp0monitor_progress.ps1"
-pause
--- optimized/launch_monitor_paper2.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-title Monitor Paper2 Power
-cd /d %~dp0
-powershell -ExecutionPolicy Bypass -File monitor_paper2_power.ps1
-pause
